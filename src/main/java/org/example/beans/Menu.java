d726649249cfcb51ccae203e14ed0bee1f85bd0f
Abriel <abrielz11@gmail.com>
2023-10-25 15:28:56 +0300

    in memory almost doen

--- src/main/java/org/example/beans/Menu.java
+++ src/main/java/org/example/beans/Menu.java
@@ -42,10 +42,12 @@ public class Menu {
                     memory.putPersonToMap(person);
                     System.out.println(memory.getDump());
                 }
+
                 case 2 -> {
                     System.out.println("If you want to remove person, enter his/her email");
                     memory.removeByEmail(scanner.nextLine());
                 }
+
                 case 3 -> System.out.println("3");
                 case 4 -> System.out.println("4");
                 case 5 -> System.out.println("5");
