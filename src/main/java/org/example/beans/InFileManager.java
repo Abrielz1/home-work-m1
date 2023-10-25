df18bebe1687ba5e1ed9e87fd2b0077e35cbd1c2
Abriel <abrielz11@gmail.com>
2023-10-25 21:35:50 +0300

    almost complete save is done loading in done

--- src/main/java/org/example/beans/InFileManager.java
+++ src/main/java/org/example/beans/InFileManager.java
@@ -1,26 +1,20 @@
 package org.example.beans;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.example.exceptions.ManagerSaveException;
 import org.example.model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import static java.util.Collections.addAll;
-
-//@Scope("singleton")
+@Scope("singleton")
 @Component
 @Data
 @Primary
@@ -39,12 +33,9 @@ public class InFileManager implements EnvMemoryHolder {
         this.manager = manager;
     }
 
-    //todo логику считывания из файла
-
     private String[] readFromFile() {
         try {
-
-           return Files.readString(Path.of(PATH), StandardCharsets.UTF_8).split(System.lineSeparator());
+            return Files.readString(Path.of(PATH), StandardCharsets.UTF_8).split(System.lineSeparator());
         } catch (IOException err) {
             throw new ManagerSaveException("Ошибка при восстановлении данных");
         }
@@ -53,17 +44,18 @@ public class InFileManager implements EnvMemoryHolder {
 
     @Override
     public void loadTasks() {
-       String[] lines = readFromFile();
 
-       for (int i = 0; i < lines.length; i++) {
-           String line = lines[i];
-           String[] content = line.split("\\;");
-           String fullName = content[0];
-           String phoneNumber = content[1];
-           String email = content[2];
-           Person person = new Person(fullName, phoneNumber, email);
-           manager.putPersonToMap(person);
-       }
+        String[] lines = readFromFile();
+
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            String[] content = line.split("\\;");
+            String fullName = content[0];
+            String phoneNumber = content[1];
+            String email = content[2];
+            Person person = new Person(fullName, phoneNumber, email);
+            manager.putPersonToMap(person);
+        }
     }
 
 
@@ -74,7 +66,7 @@ public class InFileManager implements EnvMemoryHolder {
             Path path = Path.of(PATH);
             String data = personToString() + System.lineSeparator();
 
-           Files.writeString(path, data);
+            Files.writeString(path, data);
         } catch (IOException e) {
             throw new ManagerSaveException("Ошибка, при записи файла произошел сбой!");
         }
@@ -84,12 +76,12 @@ public class InFileManager implements EnvMemoryHolder {
         StringBuilder stringBuilder = new StringBuilder();
         List<Person> taskDump = new ArrayList<>(manager.getDump().values());
         System.out.println(taskDump.size());
-            for (Person i: taskDump) {
-                stringBuilder.append(i.getFullName()).append(";")
-                        .append(i.getPhoneNumber()).append(";")
-                        .append(i.getEmail())
-                        .append(System.lineSeparator());
-            }
+        for (Person i : taskDump) {
+            stringBuilder.append(i.getFullName()).append(";")
+                    .append(i.getPhoneNumber()).append(";")
+                    .append(i.getEmail())
+                    .append(System.lineSeparator());
+        }
         return stringBuilder.toString();
     }
 }
--- src/main/java/org/example/beans/InMemoryManager.java
+++ src/main/java/org/example/beans/InMemoryManager.java
@@ -1,14 +1,11 @@
 package org.example.beans;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,16 +33,12 @@ public class InMemoryManager implements EnvMemoryHolder {
 
     public void putPersonToMap(Person person) {
         dump.put(person.getEmail(), person);
-   //     System.out.println("person on the map, the key is: " + dump.get(person).getEmail());
     }
 
-    //todo логику загрузки из файла в мапу
     @Override
     public void loadTasks() {
-
     }
 
-    //todo логику загрузки из мапы в файл
     @Override
     public void saveTasks() {
 
