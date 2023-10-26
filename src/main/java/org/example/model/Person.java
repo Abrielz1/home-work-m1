5434f8326825d9f3d68ff1a6a239111f76c64a51
Abriel <abrielz11@gmail.com>
2023-10-26 22:16:55 +0300

    almost complete validation is almost done only profiling is left

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,7 +1,9 @@
 package org.example.model;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,10 @@ public class Person {
     private String fullName;
 
     @NotBlank
+    @Pattern(regexp = "\\+\\d(-\\d{3}){2}-\\d{4}", message = "Phone is incorrect")
     private String phoneNumber;
 
+    @Pattern(regexp = "^(.+)@(S+) $.", message = "Email is incorrect")
     @Email(message = "Некорректный email")
     private String email;
 }
