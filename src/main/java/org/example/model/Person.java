475b1ad3a0849eb300af0deb11b3e3f577d2fa18
Abriel <abrielz11@gmail.com>
2023-10-26 23:20:38 +0300

    almost complete validation is almost done only profiling is left

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,6 +1,5 @@
 package org.example.model;
 
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
